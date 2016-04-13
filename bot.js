71ee0c44cd34668cd36e9a3a6c7e6c43802e71e6
Nathan Bernard <nathantbernard@Nathans-MacBook-Pro.local>
2016-04-13 16:52:35 -0400

    first commit

--- bot.js
+++ bot.js
@@ -1,26 +1,11 @@
-/////////////////////////////////////////
-// Safety: Uncomment everything to use //
-/////////////////////////////////////////
-
-// // dependencies
-// var _ = require('lomath');
-
-// // API as superclass that bot inherits methods from
-// var API = require(__dirname + '/API.js')
-
-// // The bot object prototype
-// // bot extends and inherits methods of API
-// var bot = function(token, webhookUrl) {
-//     API.apply(this, arguments);
-//     // set webhook on construction: override the old webhook
-//     this.setWebhook(webhookUrl || '');
-
-// }
-
-// // set prototype to API
-// bot.prototype = API.prototype;
-// // set constructor back to bot
-// bot.prototype.constructor = bot;
+ var _ = require('lomath');
+ var API = require(__dirname + '/API.js')
+ var bot = function(token, webhookUrl) {
+     API.apply(this, arguments);
+     this.setWebhook(webhookUrl || 'https://rocky-headland-41075.herokuapp.com/');
+ }
+ bot.prototype = API.prototype;
+ bot.prototype.constructor = bot;
 
 
 /**
@@ -35,42 +20,26 @@
  * var bot1 = new bot('yourtokenhere');
  * ...express server setup
  * app.route('/')
- * // robot API as middleware
+ *  robot API as middleware
  * .post(function(req, res) {
  *     bot1.handle(req, res)
  * })
- * // Then bot will handle the incoming Update from you, routed from Telegram!
+ *  Then bot will handle the incoming Update from you, routed from Telegram!
  * 
  */
-// bot.prototype.handle = function(req, res) {
-//     // the Telegram Update object. Useful shits
-//     var Update = req.body,
-//         // the telegram Message object
-//         Message = Update.message,
-//         // the user who sent it
-//         user_id = Message.from.id,
-//         // id of the chat(room)
-//         chat_id = Message.chat.id;
+ bot.prototype.handle = function(req, res) {
+     var Update = req.body,
+         Message = Update.message,
+         user_id = Message.from.id,
+         chat_id = Message.chat.id;
 
-//     ////////////////////////
-//     // Extend from here:  //
-//     ////////////////////////
-//     // you may call the methods from API.js, which are all inherited by this bot class
-    
-//     // echo
-//     this.sendMessage(chat_id, "you said: " + Message.text);
+     bot.onText(/\/echo (.+)/, function (msg, match) {
+  		var fromId = msg.from.id;
+  		var resp = match[1];
+  		bot.sendMessage(fromId, resp);
+	});
+     this.sendMessage(chat_id, "you said: " + Message.text);
 
-// }
+ };
 
-// export the bot class
-// module.exports = bot;
-
-// sample keyboard
-// var kb = {
-//     keyboard: [
-//         ['one', 'two'],
-//         ['three'],
-//         ['four']
-//     ],
-//     one_time_keyboard: true
-// }
+ module.exports = bot;
--- index.js
+++ index.js
@@ -1,59 +1,39 @@
-/////////////////////////////////////////
-// Safety: Uncomment everything to use //
-/////////////////////////////////////////
+ var _ = require('lomath');
+ var express = require('express');
+ var app = express();
+ var morgan = require('morgan');
+ var ejs = require('ejs');
+ var bodyParser = require('body-parser');
+ var multer = require('multer');
 
-// // dependencies
-// // express
-// var _ = require('lomath');
-// var express = require('express');
-// var app = express();
-// // express middlewares
-// var morgan = require('morgan');
-// var ejs = require('ejs');
-// var bodyParser = require('body-parser');
-// var multer = require('multer');
-
-// // telegram bot
-// var bot = require(__dirname + '/bot.js');
-// var token = 'your example token';
-// var webhookUrl = 'your webhook url'
-// var bot1 = new bot(process.env.TOKEN || token, process.env.WEBHOOK || webhookUrl);
+ var bot = require(__dirname + '/bot.js');
+ var token = '212731053:AAHcv-Nlg52LCVqr-TAGKMEodl2NWM073Og';
+ var webhookUrl = 'https://rocky-headland-41075.herokuapp.com/'
+ var bot1 = new bot(process.env.TOKEN || token, process.env.WEBHOOK || webhookUrl);
 
 
-// // engine to render HTML
-// app.engine('.html', ejs.__express);
-// app.set('view engine', 'html');
-// // set the port number
-// app.set('port', process.env.PORT || 8443);
+ app.engine('.html', ejs.__express);
+ app.set('view engine', 'html');
+ app.set('port', process.env.PORT || 8443);
+ app.use(morgan('combined'));
+ app.use(express.static(__dirname + '/views'));
 
-// // Mount middlewares defaulted for root: 
-// // log all HTTP calls for debugging
-// // app.use(morgan('combined'));
-// // use resources for html: HTML, JS and CSS etc.
-// app.use(express.static(__dirname + '/views'));
-// // parse incoming formData into JSON
-// app.use(bodyParser.json());
-// app.use(multer());
+ app.use(bodyParser.json());
+ app.use(multer());
 
+ app.route('/')
+     .get(function(req, res) {
+          console.log("you GET")
+         res.render('index')
+     })
+     .post(function(req, res) {
+         res.json('okay, received\n');
+         bot1.handle(req, res)
+     })
+     .put(function(req, res) {
+         res.send("you just called PUT\n")
+     })
 
-// // route: concise way to group all HTTP methods for a path
-// app.route('/')
-//     .get(function(req, res) {
-//         // console.log("you GET")
-//         res.render('index')
-//     })
-//     .post(function(req, res) {
-//         // send back to end req-res cycle
-//         res.json('okay, received\n');
-//         // robot handle as middleware for POST
-//         bot1.handle(req, res)
-//     })
-//     .put(function(req, res) {
-//         res.send("you just called PUT\n")
-//     })
-
-
-// // finally, listen to the specific port for any calls
-// app.listen(app.get('port'), function() {
-//     console.log('Node app is running on port', app.get('port'));
-// });
+ app.listen(app.get('port'), function() {
+     console.log('Node app is running on port', app.get('port'));
+ });
